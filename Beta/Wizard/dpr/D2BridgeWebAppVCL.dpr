89d67b4919d7356ab96627445612e317e0b4205a
Hamid reza Kabiri <h_kabiri@yahoo.com>
2026-02-14 12:49:49 +0300

    Add files via upload

--- Beta/Wizard/dpr/D2BridgeWebAppVCL.dpr
+++ Beta/Wizard/dpr/D2BridgeWebAppVCL.dpr
@@ -1,13 +1,21 @@
 {$IFDEF D2DOCKER}library{$ELSE}program{$ENDIF} D2BridgeWebAppVCL;
 
+{$IFNDEF D2WindowsService}
 {$IFDEF D2BRIDGE}
  //{$APPTYPE CONSOLE}
+{$ENDIF}
 {$ENDIF}
 
 {Language Support Resource}
 
 uses
+  {$IFDEF D2WindowsService}
+  Vcl.SvcMgr,
+  Unit_D2Bridge_Server_Service in 'Unit_D2Bridge_Server_Service.pas',
+  Unit_D2Bridge_Server_Core in 'Unit_D2Bridge_Server_Core.pas',  
+  {$ELSE}
   Vcl.Forms,
+  {$ENDIF}
   D2Bridge.ServerControllerBase in '{D2BridgePath}D2Bridge.ServerControllerBase.pas' {D2BridgeServerControllerBase: TDataModule},
   Prism.SessionBase in '{D2BridgePath}Prism\Prism.SessionBase.pas' {PrismSessionBase: TPrismSessionBase},
   {ProjectName}WebApp in '{ProjectName}WebApp.pas' {{ProjectName}WebAppGlobal},
@@ -20,8 +28,16 @@ uses
 {$R *.res}
 
 begin
+  {$IFDEF D2WindowsService}
+  if not Application.DelayInitialize or Application.Installing then
+    Application.Initialize;
+
+  Application.CreateForm(T{ProjectName}Service, {ProjectName}Service);
+  Application.Run;
+  {$ELSE}
   Application.Initialize;
   Application.MainFormOnTaskbar:= True;
   {Server}
   {Application.Run;}
+  {$ENDIF}
 end.
